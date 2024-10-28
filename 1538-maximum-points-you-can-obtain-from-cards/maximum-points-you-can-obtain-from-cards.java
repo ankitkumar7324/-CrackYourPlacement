c54256604f0acad21ccfe7ca6ccd59ab3330496e
Ankit Kumar <122971700+ankitkumar7324@users.noreply.github.com>
2024-10-28 16:00:09 +0530

    Time: 2 ms (99.00%) | Memory: 54.6 MB (95.45%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -1,24 +1,24 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-        int maxsum = 0;
-        int leftsum =0;
-        int rightsum=0;
         
+        int n = cardPoints.length;
+        int lsum = 0;
+        int rsum = 0;
+        int maxsum = 0;
         for(int i=0;i<k;i++)
         {
-            leftsum+=cardPoints[i];
-            
+            lsum += cardPoints[i];
         }
-        maxsum = leftsum;
-        int idx = cardPoints.length-1;
-        for(int i=k-1;i>=0;i--)
-        {
-            leftsum-=cardPoints[i];
-            rightsum+= cardPoints[idx];
-            maxsum = Math.max(maxsum,leftsum + rightsum);
-            idx--;
-        }
+        maxsum = lsum;
 
+        int ridx = n-1;
+
+        for(int i=k-1;i>=0;i--){
+            lsum -= cardPoints[i];
+            rsum += cardPoints[ridx];
+            ridx -= 1;
+            maxsum = Math.max(maxsum , lsum+rsum);
+        }
         return maxsum;
     }
 }
