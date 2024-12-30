8c51c13496f98ab930d644a8dc2e8edae81bc96d
Ankit Kumar <122971700+ankitkumar7324@users.noreply.github.com>
2024-12-30 12:05:24 +0530

    Time: 3 ms (36.93%) | Memory: 47.4 MB (67.01%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,40 +1,29 @@
 class Solution {
-    private static void dfs(int node , ArrayList<ArrayList<Integer>> adj , boolean vis[])
-    {
-        vis[node] = true;
-        for(int it : adj.get(node))
-        {
-            if(!vis[it])
-            {
+    public void dfs(int start,ArrayList<ArrayList<Integer>> adj , int vis[]){
+        vis[start] = 1;
+        for(int it : adj.get(start)){
+            if(vis[it]==0){
                 dfs(it,adj,vis);
             }
         }
     }
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int n = isConnected.length;
-        
-
-        for(int i=0;i<n;i++)
-        {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int vis[] = new int[n];
+        for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(isConnected[i][j]==1 && i!=j)
-                {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j] == 1 && i!=j){
                     adj.get(i).add(j);
                 }
             }
         }
-        boolean vis[] = new boolean[n];
-        int cnt =0;
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
-            {
+        int cnt=0;
+        for(int i = 0;i<n;i++){
+            if(vis[i] == 0){
                 dfs(i,adj,vis);
                 cnt++;
             }
