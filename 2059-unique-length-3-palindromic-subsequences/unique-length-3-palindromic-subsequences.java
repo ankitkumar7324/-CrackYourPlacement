742521d7aee024814aa5ff00e91c78d6d862aa88
Ankit Kumar <122971700+ankitkumar7324@users.noreply.github.com>
2025-01-06 08:11:42 +0530

    Time: 176 ms (66.34%) | Memory: 45.4 MB (60.02%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,44 +1,28 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
         // Arrays to store first and last occurrence of each character
-        int[] firstOccurrence = new int[26];
-        int[] lastOccurrence = new int[26];
-        
-        // Initialize the arrays with -1
-        for (int i = 0; i < 26; i++) {
-            firstOccurrence[i] = -1;
-            lastOccurrence[i] = -1;
-        }
-        
-        // Fill first and last occurrence arrays
-        for (int i = 0; i < s.length(); i++) {
-            int index = s.charAt(i) - 'a';
-            if (firstOccurrence[index] == -1) {
-                firstOccurrence[index] = i;
+        HashMap<Character,Integer> firstOcc = new HashMap<>();
+        HashMap<Character,Integer> lastOcc = new HashMap<>();
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(!firstOcc.containsKey(ch)){
+                firstOcc.put(ch,i);
             }
-            lastOccurrence[index] = i;
+            lastOcc.put(ch,i);
         }
-        
-        // HashSet to store unique palindromic subsequences
-        HashSet<String> uniquePalindromes = new HashSet<>();
-        
-        // Iterate through each character from 'a' to 'z'
-        for (int i = 0; i < 26; i++) {
-            if (firstOccurrence[i] != -1 && lastOccurrence[i] != -1 && firstOccurrence[i] < lastOccurrence[i]) {
-                int start = firstOccurrence[i];
-                int end = lastOccurrence[i];
-                
-                // Check characters between the first and last occurrence
-                for (int j = start + 1; j < end; j++) {
-                    char middleChar = s.charAt(j);
-                    String palindrome = "" + (char) (i + 'a') + middleChar + (char) (i + 'a');
-                    uniquePalindromes.add(palindrome);
-                }
+        int cnt=0;
+        for(char ch : firstOcc.keySet()){
+            int first = firstOcc.get(ch);
+            int last = lastOcc.get(ch);
+            if(first==last) continue;
+            HashSet<Character> set = new HashSet<>();
+            for(int j = first+1;j<last;j++){
+                set.add(s.charAt(j));
             }
+            cnt+=set.size();
         }
-        
-        // Return the size of the HashSet
-        return uniquePalindromes.size();
+        return cnt;
     
     }
 }
