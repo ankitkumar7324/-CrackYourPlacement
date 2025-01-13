7ca473f6740220dfec15a6b1837d722ecc902275
Ankit Kumar <122971700+ankitkumar7324@users.noreply.github.com>
2025-01-13 13:08:11 +0530

    Time: 4 ms (87.56%) | Memory: 45.5 MB (34.09%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -1,18 +1,23 @@
 class Solution {
     public boolean canConstruct(String s, int k) {
-        Map<Character, Integer> M = new HashMap<>();
-        int Single = 0;
+        int n = s.length();
+        if (n < k)
+            return false;
 
-        for (char c : s.toCharArray()) {
-            M.put(c, M.getOrDefault(c, 0) + 1);
-        }
+        int freq[] = new int[26];
 
-        for (int freq : M.values()) {
-            if (freq % 2 == 1) {
-                Single++;
-            }
-        }
+        for (int i = 0; i < n; i++)
+            freq[s.charAt(i) - 'a']++;
 
-        return Single <= k && k <= s.length();
+        int odd_count = 0;
+
+        for (int p : freq)
+            if (p % 2 == 1)
+                odd_count++;
+
+        if (odd_count > k)
+            return false;
+
+        return true;
     }
 }
