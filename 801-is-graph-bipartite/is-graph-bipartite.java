55e553d2e16ac4609708f1d3a13733778269e6df
Ankit Kumar <122971700+ankitkumar7324@users.noreply.github.com>
2025-01-16 11:59:24 +0530

    Time: 1 ms (63.43%) | Memory: 45.5 MB (17.64%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,16 +1,18 @@
 class Solution {
-    public boolean check(int start,int V , int col[],int graph[][]){
+    public boolean check(int start,int col[],int graph[][]){
         Queue<Integer> q = new LinkedList<>();
+        col[start] = 0;
         q.add(start);
-        col[start]=0;
         while(!q.isEmpty()){
             int node = q.remove();
             for(int it : graph[node]){
-                if(col[it] == -1){
-                    col[it] = 1 - col[node];
+                if(col[it]==-1){
+                    col[it]=1-col[node];
                     q.add(it);
-                }else if(col[it] == col[node]){
-                    return false;
+                }else{
+                    if(col[it]==col[node]){
+                        return false;
+                    }
                 }
             }
         }
@@ -24,7 +26,7 @@ class Solution {
         }
         for(int i=0;i<V;i++){
             if(col[i]==-1){
-                if(check(i,V,col,graph) == false){
+                if(check(i,col,graph)==false){
                     return false;
                 }
             }
